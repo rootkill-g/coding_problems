3ce8918d8d5a45899a7b2165085571103063b0ec
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2022-01-31 00:19:22 +0530

    rust template for codeforces

---
+++ forces/rust/template/target/debug/deps/template-06fa2b44a8c80b3a.d
@@ -0,0 +1,5 @@
+/home/rootkill/programming_problems/forces/rust/template/target/debug/deps/template-06fa2b44a8c80b3a.rmeta: src/main.rs
+
+/home/rootkill/programming_problems/forces/rust/template/target/debug/deps/template-06fa2b44a8c80b3a.d: src/main.rs
+
+src/main.rs:
---
+++ forces/rust/template/target/debug/deps/template-896034b87b842309.d
@@ -0,0 +1,5 @@
+/home/rootkill/programming_problems/forces/rust/template/target/debug/deps/template-896034b87b842309.rmeta: src/main.rs
+
+/home/rootkill/programming_problems/forces/rust/template/target/debug/deps/template-896034b87b842309.d: src/main.rs
+
+src/main.rs:
