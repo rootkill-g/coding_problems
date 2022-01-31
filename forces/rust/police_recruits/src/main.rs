bd522214b6933556a3809c1f8a1012181deaa5c6
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2022-01-31 07:26:08 +0530

    Solved::police_recruits

---
+++ forces/rust/police_recruits/src/main.rs
@@ -0,0 +1,65 @@
+use std::{
+    io::{self, prelude::*},
+    str,
+};
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
+
+fn solve<R: BufRead, W: Write>(scan: &mut Scanner<R>, w: &mut W) {
+    let n = scan.token::<isize>();
+    let a: Vec<isize> = (0..n).map(|_| scan.token::<isize>()).collect();
+    let mut _c: isize = 0;
+    let mut p: isize = 0;
+
+    for x in a {
+        if x > 0 {
+            p += x;
+        } else if x < 0 {
+            if p == 0 {
+                _c += 1;
+            } else {
+                p += x;
+            }
+        }
+    }
+
+    writeln!(w, "{}", _c).ok();
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    solve(&mut scan, &mut out);
+}
