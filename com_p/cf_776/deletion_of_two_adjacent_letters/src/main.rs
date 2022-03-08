d8395cf068f5c1b7d5ab9287e434720675b3e23f
rusty-h1000 <98808554+rusty-h1000@users.noreply.github.com>
2022-03-08 23:00:10 +0530

    Codeforces 776

---
+++ com_p/cf_776/deletion_of_two_adjacent_letters/src/main.rs
@@ -0,0 +1,64 @@
+use std::{
+    io::{self, prelude::*},
+    str,
+};
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+
+    fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
+
+fn solve<R: BufRead, W: Write>(scan: &mut Scanner<R>, w: &mut W) {
+    let t: usize = scan.token();
+    for _ in 0..t {
+        let s: String = scan.token();
+        let c: char = scan.token();
+        let mut flag: bool = false;
+        for (i, v) in s.chars().enumerate() {
+            if v == c && i % 2 == 0 {
+                flag = true;
+                break;
+            }
+        }
+        if flag {
+            writeln!(w, "YES").ok();
+        } else {
+            writeln!(w, "NO").ok();
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    solve(&mut scan, &mut out);
+}
